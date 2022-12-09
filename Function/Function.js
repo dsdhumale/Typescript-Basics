c642c0fe9408b80498bef72cc3bd57a7dc998729
dsdhumale <d.sdhumale@gmail.com>
2022-12-09 20:27:55 +0530

    [Devendra]Added: Function Operation in TypeScript

---
+++ Function/Function.js
@@ -0,0 +1,16 @@
+//Multifly function
+function multiplication(a, b) {
+    return a * b;
+}
+var result = multiplication(4, 5);
+console.log("Multiplication is : ", result);
+// returning nothing
+function display() {
+    console.log('Hello!');
+}
+display();
+//Square function
+function square(value) {
+    return Math.pow(value, 2);
+}
+console.log(square(10));
---
+++ Function/Function.ts
@@ -0,0 +1,23 @@
+//Multifly function
+
+function multiplication(a: number, b: number) {
+    return a * b;
+}
+let result = multiplication(4, 5)
+console.log("Multiplication is : ", result);
+
+// returning nothing
+
+function display(): void {
+    console.log('Hello!');
+}
+display()
+
+//Square function
+
+function square(value: number) {
+    return value ** 2;
+}
+
+console.log(square(10));
+
